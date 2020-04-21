ed48f60471ec1e459e885d91744fe7c39ac4dd7f
Juan Cobian <cobianjuan19@gmail.com>
2020-04-21 16:15:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,11 @@ class Song
   @@genre_count = {}
   @@artist_count = {}
   
-  def initialize
+  def initialize(name, artist, genre)
     @@count += 1
-    @@genres
-    @@artists
+    @name = name
+    @@genres = genre
+    @@artists = artist
   end
   
   def self.count
