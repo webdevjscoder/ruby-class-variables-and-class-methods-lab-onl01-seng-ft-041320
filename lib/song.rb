7519e21e7026d5a6c7c8b7bc23d2762d05b9defe
Juan Cobian <cobianjuan19@gmail.com>
2020-04-21 14:58:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,28 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genre = []
+  @@artist = []
   
   def initialize
     @@count += 1
+    @@genre
+    @@artist
   end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genre
+    @@genre
+  end
+  
+  def self.artist
+    @@artist
+  end
+  
+  
+  
   
 end
